64c2d2c0142afa73d3533726d917ad06b0c7284e
Bill Ho <ycho.bill.lab@gmail.com>
2024-08-13 18:45:47 +0800

    [Unit Test Vocabulary and Design] Demo: Test Fixtures - Using setUp and tearDown

--- test_phonebook.py
+++ test_phonebook.py
@@ -4,16 +4,26 @@ from phonebook import Phonebook
 
 
 class PhonebookTest(unittest.TestCase):
-    def test_lookup_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add("Bob", "12345")
+    def setUp(self) -> None:
+        self.phonebook = Phonebook()
 
-        number = phonebook.lookup("Bob")
+    def teardown(self) -> None:
+        pass
+
+    def test_lookup_by_name(self):
+        self.phonebook.add("Bob", "12345")
+
+        number = self.phonebook.lookup("Bob")
 
         self.assertEqual("12345", number)
 
     def test_missing_name(self):
-        phonebook = Phonebook()
+        self.phonebook = Phonebook()
 
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
+
+    def test_empty_phonebook_is_consistent(self):
+        self.phonebook = Phonebook()
+        is_consistent = self.phonebook.is_consistent()
+        self.assertTrue(is_consistent)
