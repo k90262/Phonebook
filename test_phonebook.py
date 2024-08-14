4010b8fa6e760510b163aef6adb0960b65f66a33
Bill Ho <ycho.bill.lab@gmail.com>
2024-08-14 19:32:55 +0800

    [Unit Test Vocabulary and Design] (1. refactor names, (2. Demo: Test Case Design - Names

--- test_phonebook.py
+++ test_phonebook.py
@@ -12,18 +12,23 @@ class PhonebookTest(unittest.TestCase):
 
     def test_lookup_by_name(self):
         self.phonebook.add("Bob", "12345")
-
         number = self.phonebook.lookup("Bob")
-
         self.assertEqual("12345", number)
 
-    def test_missing_name(self):
-        self.phonebook = Phonebook()
-
+    def test_lookup_by_missing_name(self):
         with self.assertRaises(KeyError):
             self.phonebook.lookup("missing")
 
-    def test_empty_phonebook_is_consistent(self):
-        self.phonebook = Phonebook()
+    def test_is_consistent_with_empty_phonebook(self):
         is_consistent = self.phonebook.is_consistent()
         self.assertTrue(is_consistent)
+
+    def test_is_consistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Anna", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "12345") # identical to Bob
+        self.assertFalse(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "123") # prefix of Bob
+        self.assertFalse(self.phonebook.is_consistent())
