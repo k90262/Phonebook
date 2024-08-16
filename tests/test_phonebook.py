1c8d3614d513eb146ac284545133520b88be0d9e
Bill Ho <ycho.bill.lab@gmail.com>
2024-08-16 18:06:49 +0800

    [Unit Test Vocabulary and Design] Demo: Writing Unit Tests Using pytest - converted from using unittest module into using pytest modle
    Under .venv environment, using `python -m pytest` should pass 6 test cases as well.

---
+++ tests/test_phonebook.py
@@ -0,0 +1,42 @@
+import pytest
+
+from src.phonebook import Phonebook
+
+
+@pytest.fixture
+def phonebook():
+    return Phonebook()
+
+
+def test_lookup_by_name(phonebook):
+    phonebook.add("Bob", "12345")
+    number = phonebook.lookup("Bob")
+    assert "12345" == number
+
+
+def test_lookup_by_missing_name(phonebook):
+    with pytest.raises(KeyError):
+        phonebook.lookup("missing")
+
+
+def test_is_consistent_with_empty_phonebook(phonebook):
+    is_consistent = phonebook.is_consistent()
+    assert is_consistent
+
+
+def test_is_consistent_with_different_entries(phonebook):
+    phonebook.add("Bob", "12345")
+    phonebook.add("Anna", "012345")
+    assert phonebook.is_consistent()
+
+
+def test_is_consistent_with_duplicate_entries(phonebook):
+    phonebook.add("Bob", "12345")
+    phonebook.add("Sue", "12345")
+    assert not phonebook.is_consistent()
+
+
+def test_is_consistent_with_duplicate_prefix(phonebook):
+    phonebook.add("Bob", "12345")
+    phonebook.add("Sue", "123")
+    assert not phonebook.is_consistent()
